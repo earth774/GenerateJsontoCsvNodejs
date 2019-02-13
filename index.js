8b9da3ec0d193d5d3550b62a779ae56583324c7f
Sutthipong Nuanma <sutthiphongnuanma@gmail.com>
2019-02-13 10:04:51 +0700

    add csv

--- index.js
+++ index.js
@@ -14,30 +14,53 @@ app.get('/', (req, res) => {
     let rawdata = fs.readFileSync('sampran-organic-social-mover-export_data.json');  
     let student = JSON.parse(rawdata);  
     let data = [];
-    for (var key in student.users) {
-        if (!student.users.hasOwnProperty(key)) continue;
-        var obj = student.users[key];
-        var moonLanding = new Date(obj.created_at);
-        let data_check_month = moonLanding.getMonth()  == 10;
-        let data_check_month1 = moonLanding.getMonth()  == 11;
-        let date_check = moonLanding.getDate()>=23
+    // for (var key in student.users) {
+    //     if (!student.users.hasOwnProperty(key)) continue;
+    //     var obj = student.users[key];
+        
+    //    /*  code for check between */
+    //     var moonLanding = new Date(obj.created_at);
+    //     let data_check_month = moonLanding.getMonth()  == 10;
+    //     let data_check_month1 = moonLanding.getMonth()  == 11;
+    //     let date_check = moonLanding.getDate()>=23
 
-        if(data_check_month){
-            if(date_check){
-                data.push(obj)
-            }
-        }
+    //     if(data_check_month){
+    //         if(date_check){
+    //             data.push(obj)
+    //         }
+    //     }
 
-        if(data_check_month1){
+    //     if(data_check_month1){
+    //             data.push(obj)
+    //     }
+    // }
+
+    for (var key in student) {
+        if (!student.hasOwnProperty(key)) continue;
+        var obj = student[key];
+        
+       /*  code for check between */
+        // var moonLanding = new Date(obj.created_at);
+        // let data_check_month = moonLanding.getMonth()  == 10;
+        // let data_check_month1 = moonLanding.getMonth()  == 11;
+        // let date_check = moonLanding.getDate()>=23
+
+        // if(data_check_month){
+        //     if(date_check){
+        //         data.push(obj)
+        //     }
+        // }
+
+        // if(data_check_month1){
                 data.push(obj)
-        }
+        // }
     }
     const csvWriter = createCsvWriter({
         path: 'path/to/file.csv',
         header: [
             {id: 'firstname', title: 'ชื่อ'},
             {id: 'lastname', title: 'นามสกุล'},
-            {id: 'tel', title: 'เบอร์โทรศัพท์'},
+            // {id: 'tel', title: 'เบอร์โทรศัพท์'},
             {id: 'email', title: 'อีเมล'},
         ]
     });
